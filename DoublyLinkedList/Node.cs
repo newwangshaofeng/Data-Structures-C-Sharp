26699cca633bb876b21b0f878a61d8c6e0544a85
Stratis Dermanoutsos <38103639+Stratis-Dermanoutsos@users.noreply.github.com>
2020-06-24 12:54:08 +0300

    Node class

---
+++ DoublyLinkedList/Node.cs
@@ -0,0 +1,39 @@
+namespace DataStructures_Extra_
+{
+    // Node of the Linked list / Chain
+    class Node<T>
+    {
+        // Member variables ~~~~~~~~~~~
+        private T value;
+        private Node<T> next;
+        private Node<T> previous;
+        // ~~~~~~~~~~~
+
+        // Constructors ~~~~~~~~~~~
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+            this.previous = null;
+        }
+        // ~~~~~~~~~~~
+
+        // Properties ~~~~~~~~~~~
+
+        // Getter for Value
+        public T Value { get { return this.value; } }
+
+        // Getter for next and previous nodes
+        public Node<T> Next 
+        { 
+            get { return this.next; }
+            set { this.next = value; }
+        }
+        public Node<T> Previous 
+        { 
+            get { return this.previous; } 
+            set { this.previous = value; }
+        }
+        // ~~~~~~~~~~~
+    }
+}
